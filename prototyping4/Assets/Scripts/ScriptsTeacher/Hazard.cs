c5bd38d1c4ef44bf44d5b15d3be74ccd6dfde707
GregBurrill <GregBurrillWeb@gmail.com>
2021-02-24 17:23:10 -0500

    updated npc and hazard (bullet) scripts

--- prototyping4/Assets/Scripts/ScriptsTeacher/Hazard.cs
+++ prototyping4/Assets/Scripts/ScriptsTeacher/Hazard.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile_basic : MonoBehaviour{
+public class Hazard : MonoBehaviour{
+
+	public int Damage = 1;
 	
 	public GameObject impactParticles;
-
 	public float bulletLife = 2f;
 	private float bulletTimer;
 
@@ -16,18 +17,15 @@ public class Projectile_basic : MonoBehaviour{
 		}
 	}
 
-
     // Update is called once per frame
     void OnTriggerEnter(Collider other){
 		StartCoroutine(ImpactResult());
 	}
     
-	
 	IEnumerator ImpactResult(){
 		GameObject boomParticles = Instantiate(impactParticles, transform.position, Quaternion.identity); 
 		yield return new WaitForSeconds(0.5f);
 		Destroy(gameObject);
-		
 	}
 	
 }
--- prototyping4/Assets/Scripts/ScriptsTeacher/NPC_player.cs
+++ prototyping4/Assets/Scripts/ScriptsTeacher/NPC_player.cs
@@ -4,16 +4,33 @@ using UnityEngine;
 
 public class NPC_player : MonoBehaviour{
 
+	//movement
+	public float rotationRate;
+
+	//weapon fire
 	public float projectileForce = 60f;
     public Transform firePoint;
     public GameObject projectilePrefab;
-
 	public float fireTime;
 	private float fireTimer;
 
+	//health stat
+	public int botHealth = 5;
+
+
 	void Start(){		
 		fireTime = Random.Range(0.1f,0.8f);
+		rotationRate = Random.Range(10,30);
 	}
+
+	void Update(){
+		void Update () {
+			transform.Rotate (new Vector3 (0, rotationRate, 0) * Time.deltaTime);
+        }
+		if (botHealth <= 0){
+			Destroy(gameObject);
+		}
+     } 
 
 
     void FixedUpdate(){
@@ -30,6 +47,15 @@ public class NPC_player : MonoBehaviour{
             Rigidbody rb = bullet.GetComponent<Rigidbody>();
             rb.AddForce(firePoint.forward * projectileForce, ForceMode.Impulse);
     }
+	
+	void OnCollionEnter(Collision other){
+		if (other.gameObject.tag == "hazard"){
+			int damage = other.gameObject.GetComponent<Hazard>().Damage;
+			botHealth -= damage;
+		}
+		
+	}
+	
 	
 	
 }
